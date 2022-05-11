7eaa4c7e2c58233c59379c5daebdb4bed499bb40
ngvuq11 <ngvuquoc.17@gmail.com>
2022-05-11 10:08:14 +0700

    fix: 🐛 fix loi cua ban Duc

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import './Title.scss';
+import styled from 'styled-components';
+import { Typography } from 'antd';
 
-function Title(props) {
+const { Title } = Typography;
+const TitleWrap = styled.div`
+  width: 100%;
+  text-align: left;
+  position: relative;
+  padding: 10px 0;
+  margin-bottom: 20px;
+  h3 {
+    margin-bottom: 0 !important;
+  }
+  &:before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 50px;
+    height: 4px;
+    background: #62929e;
+  }
+`;
+function Titles(props) {
   const title = props.title;
   return (
-    <div className='section__title'>
-      <h2>{title}</h2>
-    </div>
+    <TitleWrap>
+      <Title level={3}>{title}</Title>
+    </TitleWrap>
   );
 }
 
-export default Title;
+export default Titles;
