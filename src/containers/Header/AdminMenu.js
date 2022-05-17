30d09f48c400842b4bda1096d4bf1b7e70f937d2
ngvuq11 <ngvuquoc.17@gmail.com>
2022-05-17 22:33:00 +0700

    fix: 🐛 some bug

--- src/containers/Header/AdminMenu.js
+++ src/containers/Header/AdminMenu.js
@@ -4,8 +4,45 @@ import { MdDashboard } from 'react-icons/md';
 import { Menu } from 'antd';
 import { ContainerOutlined } from '@ant-design/icons';
 import { FormattedMessage } from 'react-intl';
+import { useHistory, useLocation } from 'react-router';
+import { split } from 'lodash';
 
 function AdminMenu(props) {
+  const history = useHistory();
+  const location = useLocation();
+  const isActive = (menuItem) => {
+    return location.pathname / split('/doctor')[1] === menuItem.path;
+  };
+  const menu = [
+    {
+      key: 1,
+      icon: <MdDashboard />,
+      text: 'Dashboard',
+      pathName: 'dashboard-doctor',
+      to: '/doctor/dashboard-doctor',
+    },
+    {
+      key: 2,
+      icon: <BsFillCalendarDateFill />,
+      text: <FormattedMessage id='menu.doctor.manage-schedule' />,
+      pathName: 'manage-schedule',
+      to: '/doctor/manage-schedule',
+    },
+    {
+      key: 3,
+      icon: <BsFillPersonFill />,
+      text: <FormattedMessage id='menu.doctor.manage-patient' />,
+      pathName: 'manage-patient',
+      to: '/doctor/manage-patient',
+    },
+    {
+      key: 4,
+      icon: <ContainerOutlined />,
+      text: <FormattedMessage id='menu.doctor.manage-medical-record' />,
+      pathName: 'manage-medical-record',
+      to: '/doctor/manage-medical-record',
+    },
+  ];
   return (
     <Menu theme='dark' mode='inline' defaultSelectedKeys={['1']}>
       <Menu.Item
