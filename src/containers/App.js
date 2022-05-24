92a863745a202a632c86da741a71483c091cedf6
ngvuq11 <ngvuquoc.17@gmail.com>
2022-05-24 11:24:14 +0700

    refactor: 💡 app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -51,7 +51,7 @@ class App extends Component {
         <Router history={history}>
           <CustomScrollbars style={{ height: '100vh', width: '100%' }}>
             <Switch>
-              <Route path={path.HOME} exact component={HomePage} />
+              <Route path={path.HOME} exact component={Home} />
               <Route
                 path={path.LOGIN}
                 component={userIsNotAuthenticated(Login)}
